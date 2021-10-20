15e92562a614cdf45b69698dce9135edb4f854bb
Steve Davis <stdavis@adobe.com>
2021-10-19 19:15:32 -0700

    Create migrate script

---
+++ scripts/migrate.ts
@@ -0,0 +1,17 @@
+import { createConnection } from 'typeorm';
+
+createConnection({
+  type: 'mariadb',
+  host: 'coffee.local',
+  port: 3306,
+  username: 'norman',
+  password: 'norman',
+  database: 'norman',
+  entities: ['src/models/*.ts'],
+  synchronize: true,
+})
+  .then(() => {
+    console.log('Database has been migrated');
+    process.exit();
+  })
+  .catch((error) => console.log(error));
--- src/models/Coffee.ts
+++ src/models/Coffee.ts
@@ -35,6 +35,9 @@ export class Coffee {
   @Column()
   purchaseUrl: string;
 
+  @Column({
+    nullable: true,
+  })
   @Column()
   rating: number;
 
