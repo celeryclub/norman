1cddff520d2a8824f2ad32fa1db03dbd3c793962
Steve Davis <stdavis@adobe.com>
2021-10-25 14:28:11 -0700

    Use HtmlWebpackPlugin without a template

--- client/app.tsx
+++ client/app.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const element = <h1>Hello, world</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+const rootNode = document.createElement('div');
+document.body.appendChild(rootNode);
+ReactDOM.render(element, rootNode);
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './client/index.html',
+      title: 'Norman',
     }),
   ],
 };
